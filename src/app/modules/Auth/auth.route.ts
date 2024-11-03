da2a8fefbc65e912d7f7484e5a43ecd8446ab3a3
billalhossain12 <billalhossain343434@gmail.com>
2024-11-03 13:17:21 +0600

    updated auth controller

--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -2,30 +2,32 @@ import express from 'express';
 import auth from '../../middlewares/auth';
 import { USER_ROLE } from '../User/user.constant';
 import { AuthControllers } from './auth.controller';
+import validateRequest, { validateRequestCookies } from '../../middlewares/validateRequest';
+import { AuthValidation } from './auth.validation';
 
 const router = express.Router();
 
 router.post(
   '/register',
-  // validateRequest(AuthValidation.registerValidationSchema),
+  validateRequest(AuthValidation.registerValidationSchema),
   AuthControllers.registerUser,
 );
 router.post(
   '/login',
-  // validateRequest(AuthValidation.loginValidationSchema),
+  validateRequest(AuthValidation.loginValidationSchema),
   AuthControllers.loginUser,
 );
 
 router.post(
   '/change-password',
   auth(USER_ROLE.USER, USER_ROLE.ADMIN),
-  // validateRequest(AuthValidation.changePasswordValidationSchema),
+  validateRequest(AuthValidation.changePasswordValidationSchema),
   AuthControllers.changePassword,
 );
 
 router.post(
   '/refresh-token',
-  // validateRequestCookies(AuthValidation.refreshTokenValidationSchema),
+  validateRequestCookies(AuthValidation.refreshTokenValidationSchema),
   AuthControllers.refreshToken,
 );
 
